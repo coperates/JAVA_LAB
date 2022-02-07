c3d75467ffc53c45a81325c11d6e9165aee5166d
coperates <dlwoals1195@gmail.com>
2022-02-07 12:15:06 +0900

    study Constant

---
+++ java_lab/Chapter2/src/chapter2/Constant.java
@@ -0,0 +1,21 @@
+package chapter2;
+
+public class Constant {
+
+	public static void main(String[] args) {
+		
+		final int MAX_NUM = 100;
+		final float PI = 3.14F;
+		
+		final int STUDENT_NUM = 30;
+		
+		int num = 0;
+		
+		if (num == STUDENT_NUM){}
+		System.out.println(STUDENT_NUM);
+		
+		//상수는 변하지않는 수, 상수를 재활용하여 코드수정에 용이하게 사용할 수 있다.
+
+	}
+
+}
